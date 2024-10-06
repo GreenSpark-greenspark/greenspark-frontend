be931165f6be30540e76be7b3c475c1acc883fc2
hyun907 <seunghyun020907@naver.com>
2024-10-06 20:17:35 +0900

    init : 폴더 구조 정리, 불필요한 파일 삭제

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "GreenSpark",
+  description: "그린스파크",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>
+        <div>레이아웃</div>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/power/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Power() {
+  return (
+    <div>
+      Power 페이지
+    </div>
+  )
+}
