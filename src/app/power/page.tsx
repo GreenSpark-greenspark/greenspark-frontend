249f7d29ed890c3fa6cee6a9f38be062a297f683
xionng <leeso0717@naver.com>
2024-10-09 03:11:32 +0900

    style: 공통 css 제작

--- src/app/power/page.tsx
+++ src/app/power/page.tsx
@@ -1,17 +1,25 @@
+"use client";
 import React from "react";
 
 import NowPower from "../_component/power/NowPower";
 import AICharge from "../_component/power/AICharge";
 import GraphCharge from "../_component/power/GraphCharge";
 import PreCharge from "../_component/power/PreCharge";
+import styled from "styled-components";
 
 export default function Power() {
   return (
     <div>
-      <NowPower />
-      <AICharge />
-      <GraphCharge />
-      <PreCharge />
+      <PagesWrap>
+        <NowPower />
+        <AICharge />
+        <GraphCharge />
+        <PreCharge />
+      </PagesWrap>
     </div>
   );
 }
+const PagesWrap = styled.div`
+  margin-top: 4rem;
+  margin-bottom: 4rem;
+`;
