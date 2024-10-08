9aed7f7f8c3607a9b8b1f69fddfed367eb33d4e1
xionng <leeso0717@naver.com>
2024-10-08 20:37:01 +0900

    feat: 버튼 클릭시 아이콘 변경

--- src/components/bottomNav.tsx
+++ src/components/bottomNav.tsx
@@ -1,6 +1,7 @@
 "use client";
 import "@/styles/globals.css";
 import Image from "next/image";
+import { useState } from "react";
 
 import IconPower from "../../public/icon/nav_power.svg";
 import IconBook from "../../public/icon/nav_book.svg";
@@ -15,87 +16,110 @@ import IconClickHome from "../../public/icon/nav_home_click.svg";
 import IconClickMy from "../../public/icon/nav_my_click.svg";
 
 import styled from "styled-components";
-export default function bottomNav() {
+
+export default function BottomNav() {
+  const [isPowerActive, setPowerActive] = useState(false);
+  const [isBookActive, setBookActive] = useState(false);
+  const [isApplianceActive, setApplianceActive] = useState(false);
+  const [isHomeActive, setHomeActive] = useState(false);
+  const [isMyActive, setMyActive] = useState(false);
+
+  // 각 버튼 클릭 시 상태를 토글하는 함수
+  const togglePower = () => setPowerActive(!isPowerActive);
+  const toggleBook = () => setBookActive(!isBookActive);
+  const toggleAppliance = () => setApplianceActive(!isApplianceActive);
+  const toggleHome = () => setHomeActive(!isHomeActive);
+  const toggleMy = () => setMyActive(!isMyActive);
+
   return (
-    <>
-      <NavContainer>
-        <NavBtn>
-          <Image src={IconPower} alt="로고 이미지" width={60} height={60} />
-          <MenuName>파워</MenuName>
-        </NavBtn>
-        <NavBtn>
-          <Image src={IconBook} alt="로고 이미지" width={60} height={60} />
-          <MenuName>백과</MenuName>
-        </NavBtn>
-        <MenuHomeContainer>
-          <MenuHome>
-            <Image src={IconClickHome} alt="로고 이미지" width={27} height={27} />
-          </MenuHome>
-        </MenuHomeContainer>
-        <NavBtn>
-          <Image src={IconAppliance} alt="로고 이미지" width={60} height={60} />
-          <MenuName>가전</MenuName>
-        </NavBtn>
-        <NavBtn>
-          <Image src={IconMy} alt="로고 이미지" width={60} height={60} />
-          <MenuName>마이</MenuName>
-        </NavBtn>
-      </NavContainer>
-    </>
+    <NavContainer>
+      <NavBtn onClick={togglePower}>
+        <NavIcon src={isPowerActive ? IconClickPower : IconPower} alt="Power Icon" />
+        <MenuName isActive={isPowerActive}>파워</MenuName>
+      </NavBtn>
+      <NavBtn onClick={toggleBook}>
+        <NavIcon src={isBookActive ? IconClickBook : IconBook} alt="Book Icon" />
+        <MenuName isActive={isBookActive}>백과</MenuName>
+      </NavBtn>
+      <MenuHomeContainer>
+        <MenuHome isHomeActive={isHomeActive} onClick={toggleHome}>
+          <NavHomeIcon src={isHomeActive ? IconClickHome : IconHome} alt="Home Icon" />
+        </MenuHome>
+      </MenuHomeContainer>
+      <NavBtn onClick={toggleAppliance}>
+        <NavIcon
+          src={isApplianceActive ? IconClickAppliance : IconAppliance}
+          alt="Appliance Icon"
+        />
+        <MenuName isActive={isApplianceActive}>가전</MenuName>
+      </NavBtn>
+      <NavBtn onClick={toggleMy}>
+        <NavIcon src={isMyActive ? IconClickMy : IconMy} alt="My Icon" />
+        <MenuName isActive={isMyActive}>마이</MenuName>
+      </NavBtn>
+    </NavContainer>
   );
 }
+
+const NavIcon = styled(Image)`
+  width: 60px;
+  height: 60px;
+`;
+
+const NavHomeIcon = styled(Image)`
+  width: 27px;
+  height: 27px;
+`;
+
 const NavContainer = styled.div`
-    background-color: #fff;
-    box-shadow: 0px 0px 15px 0px var(--Main-Color, #D7F3C6);
-    /* height: 7.4rem; */
-    height: 74px;
-    border-radius: 30px 30px 0px 0px;
-    padding: 0 1.5rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  background-color: #fff;
+  box-shadow: 0px 0px 15px 0px var(--Main-Color, #D7F3C6);
+  height: 74px;
+  border-radius: 30px 30px 0px 0px;
+  padding: 0 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
+
 const NavBtn = styled.div`
-    height: 100%;
-    width: 6rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: #fff;
+  height: 100%;
+  width: 6rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  cursor: pointer;
 `;
-const Icon = styled.img`
-    width: 2.4rem;
-    height: 2.4rem;
-    background-color: #fff;
-    margin-bottom: 7px;
+
+const MenuName = styled.p<{ isActive: boolean }>`
+  position: relative;
+  top: -1rem;
+  width: 2rem;
+  font-size: 10px;
+  font-weight: 400;
+  text-align: center;
+  color: ${({ isActive }) => (isActive ? "black" : "gray")}; 
 `;
-const MenuName = styled.p`
-    position: relative;
-    top: -1rem;
-    width: 2rem;
-    font-size: 10px;
-    font-weight: 400;
-    text-align: center;
-`;
+
 const MenuHomeContainer = styled.div`
-    width: 7rem;
-    display: flex;
-    justify-content: center;
+  width: 7rem;
+  display: flex;
+  justify-content: center;
 `;
-const MenuHome = styled.div`
-    position: relative;
-    top: -1.5rem;
-    background-color: #91E26B;
-    /* background-color: #fff; */
-    border: #91E26B 1px solid;
-    box-shadow: 0px 0px 15px 0px var(--Main-Color, #D7F3C6);
-    display: flex;
-    width: 3.3rem;
-    height: 3.3rem; 
-    /* padding: 0.8rem; */
-    justify-content: center;
-    align-items: center;
-    border-radius: 50%;
-    
+
+const MenuHome = styled.div<{ isHomeActive: boolean }>`
+  position: relative;
+  top: -1.5rem;
+  background-color: ${({ isHomeActive }) => (isHomeActive ? "#91e26b" : "white")};
+  border: #91e26b 1px solid;
+  box-shadow: 0px 0px 15px 0px var(--Main-Color, #D7F3C6);
+  display: flex;
+  width: 3.3rem;
+  height: 3.3rem;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  cursor: pointer;
 `;
