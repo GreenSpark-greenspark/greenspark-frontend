98d0687c232953c06aa63af5f6aa80adc06e6f08
xionng <leeso0717@naver.com>
2024-10-15 02:23:59 +0900

    feat: input 버튼 추가

--- src/app/_component/power/PowerInput.tsx
+++ src/app/_component/power/PowerInput.tsx
@@ -3,8 +3,10 @@ import "./power.common.css";
 export default function NowPower() {
   return (
     <div className="wrap">
-      <p className="title">이번달 파워</p>
-      <div className="box"></div>
+      <div className="box powerinput">
+        <p className="text_normal">내 파워를 입력해주세요!</p>
+        <button className="btn">입력하러 가기</button>
+      </div>
     </div>
   );
 }
