6c3c6bfc38bd21def183b07905a5cfc822460a71
xionng <leeso0717@naver.com>
2024-10-15 03:26:17 +0900

    feat: table 컴포넌트 추가

---
+++ src/app/_component/powerinput/PowerTable.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useTable } from "react-table";
+
+export default function PowerTable() {
+  return (
+    <div>
+      <p>테이블</p>
+    </div>
+  );
+}
--- src/app/powerinput/page.tsx
+++ src/app/powerinput/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import React from "react";
 import styled from "styled-components";
+import PowerTable from "../_component/powerinput/PowerTable";
 
 export default function Page() {
   return (
     <>
       <ChartTitle>+ 버튼을 눌러 입력할 수 있어요!</ChartTitle>
+      <PowerTable />
     </>
   );
 }
