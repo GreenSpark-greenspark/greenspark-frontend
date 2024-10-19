b2fa03b9a6b05d787d3d899df418ed8f1decf521
hyun907 <seunghyun020907@naver.com>
2024-10-19 12:03:11 +0900

    feat: 공통 Top Bar 적용되도록 레이아웃 파일 변경

--- src/app/appliances/(with-my-appliances)/page.tsx
+++ src/app/appliances/(with-my-appliances)/page.tsx
@@ -1,10 +1,8 @@
 import MyAppliances from "@/app/_component/appliances/MyAppliances";
-import TopBar from "@/components/TopBar";
 
 export default function Page() {
   return (
     <>
-      <TopBar text={"내 가전제품"} />
       <MyAppliances />
     </>
   );
