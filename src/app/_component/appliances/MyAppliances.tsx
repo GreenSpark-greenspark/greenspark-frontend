950fda9f7d3e2fd42f7fd978057d01ef41b1a22e
hyun907 <seunghyun020907@naver.com>
2024-10-19 11:56:13 +0900

    feat: 가전 제품 상세 페이지 연결

--- src/app/_component/appliances/MyAppliances.tsx
+++ src/app/_component/appliances/MyAppliances.tsx
@@ -2,13 +2,16 @@ import style from "./MyAppliances.module.css";
 import IconPlus from "@/../public/icon/power_plus.svg";
 import { data } from "@/mock/appliances";
 import Appliance from "./Appliance";
+import Link from "next/link";
 
 export default function Box() {
   return (
     <div className={style.BoxWrapper}>
       <div className={style.Box}>
         {data.map(item => (
-          <Appliance key={item.id} id={item.id} grade={item.grade} type={item.type} />
+          <Link href={`/appliances/${item.id}`}>
+            <Appliance id={item.id} grade={item.grade} type={item.type} />
+          </Link>
         ))}
         <div className={style.AddCircle}>
           <IconPlus width={"2.4rem"} height={"2.4rem"} />
