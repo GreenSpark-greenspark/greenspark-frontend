b7783d8c244b3c19e328654d280556d5c0f870b1
xionng <leeso0717@naver.com>
2024-10-23 02:06:30 +0900

    feat: 전환버튼 제작

--- src/app/_component/power/GraphCharge.tsx
+++ src/app/_component/power/GraphCharge.tsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@/components/Box";
 import styles from "./power.common.module.css";
+import graphStyles from "./graph.common.module.css";
+
+import BillGraph from "./BillGraph";
+import UsageGraph from "./UsageGraph";
 
 export default function GraphCharge() {
+  const [activeButton, setActiveButton] = useState<string>("bill");
+
+  const handleButtonClick = (button: string) => {
+    setActiveButton(button);
+  };
+
   return (
     <>
-      <p className={styles.title}>한눈에 보는 내 파워</p>
+      <p className={styles.title}>한 눈에 보는 내 파워</p>
       <div className={styles.wrap}>
-        <Box>그래프</Box>
+        <Box>
+          <div className={graphStyles.btnContainer}>
+            <div
+              className={`${graphStyles.graphBtn} ${activeButton === "bill" ? graphStyles.btnActive : ""}`}
+              onClick={() => handleButtonClick("bill")}
+            >
+              <p>전기요금</p>
+            </div>
+
+            <div
+              className={`${graphStyles.graphBtn} ${activeButton === "usage" ? graphStyles.btnActive : ""}`}
+              onClick={() => handleButtonClick("usage")}
+            >
+              <p>전력사용량</p>
+            </div>
+          </div>
+          {activeButton === "bill" && <BillGraph />}
+          {activeButton === "usage" && <UsageGraph />}
+        </Box>
       </div>
     </>
   );
