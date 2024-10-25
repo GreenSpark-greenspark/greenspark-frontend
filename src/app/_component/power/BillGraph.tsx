80c345ecaf32ac28cc87c923b06689fef5723d42
xionng <leeso0717@naver.com>
2024-10-26 02:43:10 +0900

    fix: 목데이터 파일 추가

--- src/app/_component/power/BillGraph.tsx
+++ src/app/_component/power/BillGraph.tsx
@@ -12,6 +12,7 @@ import {
   Filler
 } from "chart.js";
 import { ChartData, ChartOptions } from "chart.js";
+import { powerGraph } from "@/mock/powerGraph";
 
 // Chart.js에서 필요한 컴포넌트들을 등록
 ChartJS.register(
@@ -75,28 +76,6 @@ const createRecent24MonthsData = (data: any[], currentYear: number): (number | n
   return result;
 };
 
-const exampleData = [
-  { year: 2024, month: 1, value: 8000 },
-  { year: 2024, month: 2, value: 9000 },
-  { year: 2024, month: 3, value: 8500 },
-  { year: 2024, month: 4, value: 9850 },
-  { year: 2024, month: 5, value: 7600 },
-  { year: 2024, month: 7, value: 8850 },
-  { year: 2024, month: 8, value: 8430 },
-  { year: 2024, month: 10, value: 8440 },
-  { year: 2023, month: 12, value: 7970 },
-  { year: 2023, month: 11, value: 7530 },
-  { year: 2023, month: 10, value: 6690 },
-  { year: 2023, month: 8, value: 8640 },
-  { year: 2023, month: 7, value: 8210 },
-  { year: 2023, month: 6, value: 7770 },
-  { year: 2023, month: 5, value: 6970 },
-  { year: 2023, month: 1, value: 6650 },
-  { year: 2023, month: 2, value: 7790 },
-  { year: 2022, month: 11, value: 7020 },
-  { year: 2022, month: 12, value: 6600 }
-];
-
 const BillGraph = () => {
   const scrollContainerRef = useRef<HTMLDivElement>(null);
 
@@ -109,7 +88,7 @@ const BillGraph = () => {
 
   const currentYear = new Date().getFullYear();
 
-  const recent24MonthsData = createRecent24MonthsData(exampleData, currentYear);
+  const recent24MonthsData = createRecent24MonthsData(powerGraph, currentYear);
 
   // 올해, 작년 데이터 추출
   const thisYearData = recent24MonthsData.slice(12, 24);
---
+++ src/mock/powerGraph.ts
@@ -0,0 +1,21 @@
+export const powerGraph = [
+  { year: 2024, month: 1, value: 8000 },
+  { year: 2024, month: 2, value: 9000 },
+  { year: 2024, month: 3, value: 8500 },
+  { year: 2024, month: 4, value: 9850 },
+  { year: 2024, month: 5, value: 7600 },
+  { year: 2024, month: 7, value: 8850 }, // 6월 데이터 없음
+  { year: 2024, month: 8, value: 8430 },
+  { year: 2024, month: 10, value: 8440 },
+  { year: 2023, month: 12, value: 7970 },
+  { year: 2023, month: 11, value: 7530 },
+  { year: 2023, month: 10, value: 6690 },
+  { year: 2023, month: 8, value: 8640 },
+  { year: 2023, month: 7, value: 8210 },
+  { year: 2023, month: 6, value: 7770 },
+  { year: 2023, month: 5, value: 6970 },
+  { year: 2023, month: 1, value: 6650 },
+  { year: 2023, month: 2, value: 7790 },
+  { year: 2022, month: 11, value: 7020 },
+  { year: 2022, month: 12, value: 6600 }
+];
