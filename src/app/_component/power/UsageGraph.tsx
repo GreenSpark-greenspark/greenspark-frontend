54e9b20dec2ae5b3f180cf0dde8ec23a924e9db8
xionng <leeso0717@naver.com>
2024-10-26 16:54:41 +0900

    feat: 그래프 컴포넌트로 분리

--- src/app/_component/power/UsageGraph.tsx
+++ src/app/_component/power/UsageGraph.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import Graph from "./Graph"; // Adjust the import path as necessary
+import { powerGraph } from "@/mock/powerGraph";
 
-export default function UsageGraph() {
-  return <>UsageGraph</>;
-}
+const UsageGraph = () => {
+  return <Graph data={powerGraph} isBillGraph={false} />;
+};
+
+export default UsageGraph;
