584324cbc332a04d75cca8b003eadba25317e4a7
xionng <leeso0717@naver.com>
2024-10-30 17:29:06 +0900

    feat: 로그인 페이지 기본 레이아웃 지정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import IconGoogle from "@/../public/icon/login_google.svg";
+import styles from "./LoginPage.module.css";
 
 export default function Login() {
-  return <div>로그인 페이지</div>;
+  return (
+    <>
+      <div className={styles.loginContainer}>
+        <p className={styles.loginMent}>지금 바로 시작해보세요!</p>
+        <div className={styles.loginBtn}>
+          <IconGoogle className={styles.googleIcon} />
+          <p className={styles.googleMent}>Google 계정으로 로그인</p>
+        </div>
+      </div>
+    </>
+  );
 }
