95ba613e9ab12cf3f3141898112f19ab79e56ea3
xionng <leeso0717@naver.com>
2024-11-09 23:02:42 +0900

    fix: 파워페이지 스타일드컴포넌트 제거

--- src/app/(common-layout)/power/page.tsx
+++ src/app/(common-layout)/power/page.tsx
@@ -5,21 +5,16 @@ import PowerInput from "../../_component/power/PowerInput";
 import AICharge from "../../_component/power/AICharge";
 import GraphCharge from "../../_component/power/GraphCharge";
 import PreCharge from "../../_component/power/PreCharge";
-import styled from "styled-components";
 
 export default function Power() {
   return (
-    <div style={{ height: "100vh", overflowY: "scroll" }}>
-      <PagesWrap>
-        <PowerInput />
-        <AICharge />
-        <GraphCharge />
-        <PreCharge />
-      </PagesWrap>
+    <div
+      style={{ height: "100vh", overflowY: "scroll", paddingBottom: "10rem", marginTop: "4rem" }}
+    >
+      <PowerInput />
+      <AICharge />
+      <GraphCharge />
+      <PreCharge />
     </div>
   );
 }
-const PagesWrap = styled.div`
-  margin-top: 4rem;
-  padding-bottom: 10rem;
-`;
