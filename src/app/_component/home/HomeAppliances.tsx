eeb8a8a484848683120895141860c7a568ddc17f
hyun907 <seunghyun020907@naver.com>
2024-11-09 20:00:21 +0900

    feat: 홈 페이지-내 가전제품 API 연결 및 퍼블리싱

--- src/app/_component/home/HomeAppliances.tsx
+++ src/app/_component/home/HomeAppliances.tsx
@@ -1,14 +1,68 @@
-import React from "react";
-import styles from "../power/power.common.module.css";
+"use client";
+import React, { useEffect, useState } from "react";
+import style from "./HomeAppliances.module.css";
 import Box from "@/components/Box";
+import axios from "axios";
+import Link from "next/link";
+import Appliance from "../appliances/Appliance";
+import GradeLabel from "../appliances/GradeLabel";
+import IconPlus from "@/../public/icon/home_plus.svg";
+interface ApplianceData {
+  applianceId: number;
+  grade: string;
+  matchTerm: string;
+  updated?: boolean;
+}
 
 export default function HomeAppliances() {
+  const [data, setData] = useState<ApplianceData[]>([]);
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const userId = 1;
+
+  useEffect(() => {
+    const fetchCostData = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/appliances/preview/${userId}`);
+        if (response.data.success) {
+          setData(response.data.data);
+          console.log(response.data.data);
+        } else {
+          console.error("API 호출 실패:", response.data.message);
+        }
+      } catch (error) {
+        console.error("API 호출 중 오류 발생:", error);
+      }
+    };
+
+    fetchCostData();
+  }, [API_URL, userId]);
+
   return (
     <>
-      <p className={styles.title}>내 가전제품 </p>
-
-      <div className={styles.wrap}>
-        <Box>가전 컴포넌트</Box>
+      <p className={style.title}>내 가전제품 </p>
+      <div className={style.wrap}>
+        <Box minHeight={"113px"}>
+          <div className={style.flexLayout}>
+            <div className={style.flexLayout}>
+              {data.map(item => (
+                <Link href={`/appliances/${item.applianceId}`} key={item.applianceId}>
+                  <div className={style.LinkContainer}>
+                    <Appliance id={item.applianceId} grade={item.grade} type={item.matchTerm} />
+                    <GradeLabel grade={item.grade} />
+                  </div>
+                </Link>
+              ))}
+            </div>
+            <Link href={`/appliances/add`}>
+              <div className={style.LinkContainer}>
+                <div className={style.AddCircle}>
+                  <IconPlus width={"1.2rem"} height={"1.2rem"} />
+                </div>
+                <div className={style.placeholder}></div>
+              </div>
+            </Link>
+          </div>
+        </Box>
       </div>
     </>
   );
