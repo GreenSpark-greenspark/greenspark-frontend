fd50bda890a261483d6b2c6244bf2806bf15b930
xionng <leeso0717@naver.com>
2024-11-10 18:35:15 +0900

    feat: 예상요금 안내 멘트 추가

--- src/app/_component/power/ExpectPreCost.tsx
+++ src/app/_component/power/ExpectPreCost.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import InfoIcon from "@/../public/icon/toast_info_icon.svg";
+import styles from "./expectPreCost.module.css";
 
 export default function ExpectPreCost() {
   return (
     <>
-      <div>
-        <p>ㅇㅇㅇ님의 다음 전기요금은...</p>
+      <div className={styles.expectTop}>
+        <div className={styles.topLeft}>
+          <p className={styles.topTitle}>ㅇㅇㅇ님의 이번 전기요금은...</p>
+          <div className={styles.infoContianer}>
+            <InfoIcon className={styles.infoIcon} />
+            <p className={styles.infoText}>
+              AI 기반의 예측이므로, 정확하지 않을 수 있습니다. <br />
+              더욱 정확한 예측을 원한다면 파워 빈칸을 채워주세요.
+            </p>
+          </div>
+        </div>
+        <div className={styles.topRight}>
+          <div className={styles.infoDate}></div>
+          <p className={styles.infoText}>납부일 4일</p>
+        </div>
       </div>
     </>
   );
