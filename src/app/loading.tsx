64659e8aeeacee7f79019ad43b30bbc1352723b2
hyun907 <seunghyun020907@naver.com>
2024-11-14 21:59:17 +0900

    feat: 3등급 글씨색 조건부 변경 및 loading 페이지 생성

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import LoadingDots from "@/components/LoadingDots";
+
+export default function Loading() {
+  return (
+    <div
+      style={{ height: "500px", display: "flex", justifyContent: "center", alignItems: "center" }}
+    >
+      <LoadingDots />
+    </div>
+  );
+}
