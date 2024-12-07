1f96e2b96eab5646835e977ec45c0d45d396d49c
hyun907 <seunghyun020907@naver.com>
2024-12-08 02:14:51 +0900

    feat: info 팁 아이콘 추가

--- src/app/_component/appliances/[id]/BottomView.tsx
+++ src/app/_component/appliances/[id]/BottomView.tsx
@@ -1,21 +1,33 @@
 import style from "./BottomView.module.css";
 import ItemRow from "./ItemRow";
+import { useState } from "react";
 
 interface BottomViewProps {
   [key: string]: any;
 }
 
 const BottomView = ({ ...applianceDetails }: BottomViewProps) => {
+  const [selectedKey, setSelectedKey] = useState<string | null>(null);
+
   const excludedKeys = ["업체명칭", "기자재명칭", "모델명", "효율등급"];
   const entries = Object.entries(applianceDetails)
     .filter(([key]) => !excludedKeys.includes(key))
     .filter(([key, value]) => value !== null && value !== "NULL");
 
+  const handleIconClick = (key: string) => {
+    setSelectedKey(selectedKey === key ? null : key);
+  };
+
   return (
     <div className={style.container}>
       {entries.map(([key, value], index) => (
         <div key={key + index}>
-          <ItemRow label={key} value={value} />
+          <ItemRow
+            label={key}
+            value={value}
+            isSelected={selectedKey === key}
+            onIconClick={() => handleIconClick(key)}
+          />
           {index !== entries.length - 1 && (
             <ItemRow key={key + "empty" + index} label={""} value={""} />
           )}
--- src/app/_component/appliances/[id]/ItemRow.tsx
+++ src/app/_component/appliances/[id]/ItemRow.tsx
@@ -1,23 +1,24 @@
 import React from "react";
 import style from "./ItemRow.module.css";
-import Image from "next/image";
-import InfoIcon from "@/../public/icon/toast_info_icon.svg";
+import InfoIcon from "@/../public/icon/info_icon.svg";
+import InfoFillIcon from "@/../public/icon/info_fill_icon.svg";
+
 interface ItemRowProps {
   label: string;
   value: string;
+  isSelected?: boolean;
+  onIconClick?: () => void;
 }
 
-const ItemRow = ({ label, value }: ItemRowProps) => {
+const ItemRow = ({ label, value, isSelected, onIconClick }: ItemRowProps) => {
   return (
     <div className={style.row}>
       {label !== "" && (
-        <div className={style.iconWrapper}>
-          <InfoIcon />
+        <div className={style.iconWrapper} onClick={onIconClick}>
+          {isSelected ? <InfoFillIcon /> : <InfoIcon />}
         </div>
       )}
-      {label == "" && (
-        <div className={style.emptyDiv}/>
-      )}
+      {label === "" && <div className={style.emptyDiv} />}
       <div className={style.key}>{label}</div>
       <div className={style.value}>{value}</div>
     </div>
