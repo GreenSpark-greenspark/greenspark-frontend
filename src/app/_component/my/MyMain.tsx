3257c11c251924bc00762e8e067d506a8d914bb8
xionng <leeso0717@naver.com>
2024-12-10 16:09:14 +0900

    feat: 마이페이지 등급 카운트 반영

--- src/app/_component/my/MyMain.tsx
+++ src/app/_component/my/MyMain.tsx
@@ -24,11 +24,14 @@ export default function MyMain() {
   const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
   const [point, setPoint] = useState<number>(0);
   const [name, setName] = useState<string>("ㅇㅇ");
+  const [gradeCounts, setGradeCounts] = useState({ 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 });
+
   const openLogoutPopup = () => setIsPopupOpen(true);
   const closeLogoutPopup = () => setIsPopupOpen(false);
 
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+  // 보유 포인트
   useEffect(() => {
     const fetchPoints = async () => {
       try {
@@ -44,7 +47,7 @@ export default function MyMain() {
         console.error("포인트 데이터 가져오기 실패:", error);
       }
     };
-
+    // 사용자 이름
     const fetchUserInfo = async () => {
       try {
         const response = await axios.get(`${API_URL}/users/my`, {
@@ -59,9 +62,42 @@ export default function MyMain() {
         console.error("사용자 이름 가져오기 실패:", error);
       }
     };
+    // 가전제품
+    const fetchApplianceInfo = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/appliances`, {
+          withCredentials: true
+        });
+        if (response.data && response.data.success) {
+          const appliances = response.data.data;
+
+          const counts: { 1: number; 2: number; 3: number; 4: number; 5: number } = {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0
+          };
+          // 등급 카운트하기
+          appliances.forEach((appliance: { grade: string }) => {
+            const grade = parseInt(appliance.grade, 10);
+            if (grade >= 1 && grade <= 5) {
+              counts[grade as 1 | 2 | 3 | 4 | 5]++;
+            }
+          });
+
+          setGradeCounts(counts);
+        } else {
+          console.error("가전제품 데이터가 유효하지 않습니다.");
+        }
+      } catch (error) {
+        console.error("가전제품 데이터 가져오기 실패:", error);
+      }
+    };
 
     fetchPoints();
     fetchUserInfo();
+    fetchApplianceInfo();
   }, []);
 
   return (
@@ -95,50 +131,34 @@ export default function MyMain() {
               </div>
               <IconArrow className={styles.iconArrow} />
             </div>
-            {/* <div className={styles.alignDiv}>
-              <p className={styles.textPoint}>{point.toLocaleString()}</p>
-              <IconPoint className={styles.iconPoint} />
-            </div> */}
           </div>
           <div className={styles.bodyPoint}>
             <div className={styles.alignDiv}>
               <p className={styles.textTitle}>내 가전제품</p>
-              {/* <IconArrow className={styles.iconArrow} /> */}
             </div>
           </div>
         </div>
 
         <div className={styles.gradeBoxContainer}>
-          <div className={styles.gradeBox}>
-            <p style={{ color: getColorFromGrade("1"), fontWeight: "600", fontSize: "2.5rem" }}>
-              3
-            </p>
-            <p className={styles.gradeName}>1등급</p>
-          </div>
-          <div className={styles.gradeBox}>
-            <p style={{ color: getColorFromGrade("2"), fontWeight: "600", fontSize: "2.5rem" }}>
-              3
-            </p>
-            <p className={styles.gradeName}>2등급</p>
-          </div>
-          <div className={styles.gradeBox}>
-            <p style={{ color: getColorFromGrade("3"), fontWeight: "600", fontSize: "2.5rem" }}>
-              3
-            </p>
-            <p className={styles.gradeName}>3등급</p>
-          </div>
-          <div className={styles.gradeBox}>
-            <p style={{ color: getColorFromGrade("4"), fontWeight: "600", fontSize: "2.5rem" }}>
-              3
-            </p>
-            <p className={styles.gradeName}>4등급</p>
-          </div>
-          <div className={styles.gradeBox} style={{ border: "none" }}>
-            <p style={{ color: getColorFromGrade("5"), fontWeight: "600", fontSize: "2.5rem" }}>
-              3
-            </p>
-            <p className={styles.gradeName}>5등급</p>
-          </div>
+          {Object.entries(gradeCounts).map(([grade, count]) => (
+            <div
+              key={grade}
+              className={styles.gradeBox}
+              style={{ border: grade === "5" ? "none" : undefined }}
+            >
+              <p
+                style={{
+                  // color: getColorFromGrade(grade),
+                  color: count === 0 ? "#929292" : getColorFromGrade(grade),
+                  fontWeight: "600",
+                  fontSize: "2.5rem"
+                }}
+              >
+                {count}
+              </p>
+              <p className={styles.gradeName}>{grade}등급</p>
+            </div>
+          ))}
         </div>
       </div>
 
