325b4c4e371440a38ec5d06c40bcd1a0f285df77
hyun907 <seunghyun020907@naver.com>
2024-12-15 17:55:52 +0900

    feat: 출석체크 팝업 퍼블리싱

--- src/app/_component/home/AttendanceCoin.tsx
+++ src/app/_component/home/AttendanceCoin.tsx
@@ -1,21 +1,32 @@
-import React from "react";
-import styles from "../power/power.common.module.css";
+import React, { useState } from "react";
+import styles from "./AttendanceCoin.module.css";
 import Box from "@/components/Box";
 import Iconcoin from "@/../public/icon/coin.svg";
+import AttendanceModal from "./attendanceModal.tsx/AttendanceModal";
+
 export default function AttendanceCoin() {
+  const [click, setClick] = useState(false);
+
+  const onClick = () => {
+    setClick(prevState => !prevState);
+  };
+
   return (
     <>
       <div className={styles.coinWrap}>
-        <Box>
-          <div className={styles.coinContainer}>
-            <p className={styles.text_normal}>하루에 한 번씩 포인트를 획득하세요!</p>
-            <div className={styles.coin}>
-              <Iconcoin className={styles.coinIcon} />
-              <p className={styles.text_normal}>50</p>
+        <button className={styles.btn} onClick={onClick}>
+          <Box>
+            <div className={styles.coinContainer}>
+              <p className={styles.text_normal}>하루에 한 번씩 포인트를 획득하세요!</p>
+              <div className={styles.coin}>
+                <Iconcoin className={styles.coinIcon} />
+                <p className={styles.text_normal}>50</p>
+              </div>
             </div>
-          </div>
-        </Box>
+          </Box>
+        </button>
       </div>
+      {click && <AttendanceModal click={click} onClick={onClick} />}
     </>
   );
 }
