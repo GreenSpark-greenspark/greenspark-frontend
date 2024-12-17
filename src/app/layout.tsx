322086e37d8423588e8b8162b2c8c0510a6e3c85
hyun907 <seunghyun020907@naver.com>
2024-12-17 19:26:03 +0900

    fix: 폰트 재수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="kr" className={pretendard.variable}>
-      <body className={pretendard.variable}>
+    <html lang="kr" className={`${pretendard.variable}`}>
+      <body className={`${pretendard.variable}`}>
         <StyledComponentsRegistry>
           <ScrollReset />
           {children}
