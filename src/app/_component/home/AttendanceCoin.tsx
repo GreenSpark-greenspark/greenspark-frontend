36da63157067fbb28ea4dad1f1a028d98c7c433c
hyun907 <seunghyun020907@naver.com>
2024-12-17 13:46:20 +0900

    feat: 출석체크, 포인트 갱신 api 연결

--- src/app/_component/home/AttendanceCoin.tsx
+++ src/app/_component/home/AttendanceCoin.tsx
@@ -1,15 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./AttendanceCoin.module.css";
 import Box from "@/components/Box";
 import Iconcoin from "@/../public/icon/coin.svg";
 import AttendanceModal from "./attendanceModal.tsx/AttendanceModal";
+import axios from "axios";
+import Toast from "@/components/Toast";
 
 export default function AttendanceCoin() {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
   const [click, setClick] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
 
-  const onClick = () => {
-    setClick(prevState => !prevState);
+  const onClick = async () => {
+    if (toastMessage) {
+      setToastMessage("");
+      return;
+    }
+
+    try {
+      const res = await axios.post(`${API_URL}/users/attendance`, {}, { withCredentials: true });
+
+      if (res.data.data === true) {
+        try {
+          const res = await axios.post(
+            `${API_URL}/point/update`,
+            {
+              pointAmount: 50,
+              event: "출석체크"
+            },
+            { withCredentials: true }
+          );
+        } catch (error) {
+          console.log("포인트 갱신 오류:", error);
+        }
+        setClick(true);
+      } else {
+        setToastMessage("이미 출석 체크를 하셨습니다!");
+      }
+    } catch (error) {
+      console.log("출석 체크 오류:", error);
+    }
   };
+
+  const closeModal = () => {
+    setClick(false);
+  };
+
+  useEffect(() => {
+    if (toastMessage) {
+      const timer = setTimeout(() => {
+        setToastMessage("");
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [toastMessage]);
 
   return (
     <>
@@ -26,7 +71,8 @@ export default function AttendanceCoin() {
           </Box>
         </button>
       </div>
-      {click && <AttendanceModal click={click} onClick={onClick} />}
+      {!toastMessage && click && <AttendanceModal click={click} onClick={closeModal} />}
+      {toastMessage && <Toast message={toastMessage} />}
     </>
   );
 }
